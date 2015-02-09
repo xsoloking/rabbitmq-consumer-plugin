4647e78445264276d284564f1a46b95732e90e92
rinrinne <rinrin.ne@gmail.com>
2015-02-09 14:49:20 +0900

    et system security to callback threads
    In Jenkins, all background threads should run as ACL.SYSTEM.
    Callbacks in this plugin is called from amqp library's inner thread.
    
    Recently, Jenkins core changed thread security from SYSTEM to anonymous.
    So callbacks cannot access any Jenkins items.
    
    This patch applies ACL.SYSTEM to callbacks for extension point.

--- src/main/java/org/jenkinsci/plugins/rabbitmqconsumer/extensions/MessageQueueListener.java
+++ src/main/java/org/jenkinsci/plugins/rabbitmqconsumer/extensions/MessageQueueListener.java
@@ -1,8 +1,12 @@
 package org.jenkinsci.plugins.rabbitmqconsumer.extensions;
 
+import hudson.security.ACL;
+
 import java.util.Collection;
 import java.util.Map;
 
+import org.acegisecurity.context.SecurityContext;
+import org.acegisecurity.context.SecurityContextHolder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -80,14 +84,20 @@ public abstract class MessageQueueListener implements ExtensionPoint {
             Map<String, Object> headers,
             byte[] body) {
         LOGGER.trace("MessageQueueListener", "fireOnReceive");
-        for (MessageQueueListener l : all()) {
-            if (appId.equals(l.getAppId())) {
-                try {
-                    l.onReceive(queueName, contentType, headers, body);
-                } catch (Exception ex) {
-                    LOGGER.warn("Caught exception during calling onReceive()", ex);
+        SecurityContext old = ACL.impersonate(ACL.SYSTEM);
+        try {
+            for (MessageQueueListener l : all()) {
+                if (appId.equals(l.getAppId())) {
+                    try {
+                        l.onReceive(queueName, contentType, headers, body);
+                    } catch (Exception ex) {
+                        LOGGER.warn("Caught exception during calling onReceive()", ex);
+                    }
                 }
             }
+        }
+        finally {
+            SecurityContextHolder.setContext(old);
         }
     }
 
@@ -101,14 +111,20 @@ public abstract class MessageQueueListener implements ExtensionPoint {
      */
     public static void fireOnBind(Collection<String> appIds, String queueName) {
         LOGGER.trace("MessageQueueListener", "fireOnBind");
-        for (MessageQueueListener l : all()) {
-            if (appIds.contains(l.getAppId())) {
-                try {
-                    l.onBind(queueName);
-                } catch (Exception ex) {
-                    LOGGER.warn("Caught exception during calling onBind()", ex);
+        SecurityContext old = ACL.impersonate(ACL.SYSTEM);
+        try {
+            for (MessageQueueListener l : all()) {
+                if (appIds.contains(l.getAppId())) {
+                    try {
+                        l.onBind(queueName);
+                    } catch (Exception ex) {
+                        LOGGER.warn("Caught exception during calling onBind()", ex);
+                    }
                 }
             }
+        }
+        finally {
+            SecurityContextHolder.setContext(old);
         }
     }
 
@@ -122,14 +138,20 @@ public abstract class MessageQueueListener implements ExtensionPoint {
      */
     public static void fireOnUnbind(Collection<String> appIds, String queueName) {
         LOGGER.trace("MessageQueueListener", "fireOnUnbind");
-        for (MessageQueueListener l : all()) {
-            if (appIds.contains(l.getAppId())) {
-                try {
-                l.onUnbind(queueName);
-                } catch (Exception ex) {
-                    LOGGER.warn("Caught exception during calling onUnbind()", ex);
+        SecurityContext old = ACL.impersonate(ACL.SYSTEM);
+        try {
+            for (MessageQueueListener l : all()) {
+                if (appIds.contains(l.getAppId())) {
+                    try {
+                        l.onUnbind(queueName);
+                    } catch (Exception ex) {
+                        LOGGER.warn("Caught exception during calling onUnbind()", ex);
+                    }
                 }
             }
+        }
+        finally {
+            SecurityContextHolder.setContext(old);
         }
     }
 
